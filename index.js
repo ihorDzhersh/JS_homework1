3ceba57c10ed734aa79d82b2cbacaa2f03e8066b
ihorDzhersh <68298481+ihorDzhersh@users.noreply.github.com>
2020-07-15 21:59:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+// ************************task2********************************
+
+// console.log("Dzhersh");
+
+//**************************task3************** */
+
+// let a = 1;
+// let b = 'bla bla bla';
+// console.log('a = ' + a);
+// console.log('b = ' + b);
+
+// a = b
+// console.log('a = ' + a);
+// console.log('b = ' + b);
+
+//**************************task4************** */
+
+// let person = {
+//     name: "igor",
+//     fingersOnHands: 10,
+//     isStudentOfSoftServe: true,
+//     experiense: undefined,
+//     firstOwnApp: null
+// }
+
+
+//**************************task5************** 
+
+// const isAdult = confirm("Are you already 18?");
+// console.log(isAdult);
+
+//**************************task6************** 
+
+
+// let myScript = {
+//     name: String,
+//     sourname: String,
+//     group:  String,
+//     age: Number,
+//     isMarried: confirm("Are you merried?"),
+//     experiense: undefined,
+//     firstOwnApp: null
+// }
+
+// console.log(myScript);
+//                           // {isMarried: true, name: ƒ, sourname: ƒ, group: ƒ, age: ƒ}
+
+// myScript.name = 'igor';
+// myScript.sourname = 'dzhersh';
+// myScript.group = "JS";
+// myScript.age = 33;
+
+// console.log(myScript);
+//                           // {name: "igor", sourname: "dzhersh", group: "JS", age: 33, isMarried: true, experiense: undefined, firstOwnApp: null}
+            
+// for(let value of Object.values(myScript)){
+//     console.log(typeof(value));
+// }
+                          
+                            //string            
+                            //string
+                            //string
+                            //number
+                            //boolean
+                            //underfined
+                            //object
+
+
+//************************task7 **************** */
+
+// let login = prompt("Enter your Login", ["babaGalia"]);
+// let email = prompt("Enter your email", ["Galia@gmail.com"])
+// let password = prompt("Enter your password", ["12345"]);
+
+// alert("Dear " + login + " your email is " + email + " and your password is " + password);
+
+//******************************task 8 *************** */
+
+//   let hours = prompt("hours ", ["1"]);
+//   let days = prompt("days", ["1"] );
+//   let months = prompt("months", ["1"]);
+//   let result= hours*3600 + days*86400 + months*2592000;
+//   alert(result);
